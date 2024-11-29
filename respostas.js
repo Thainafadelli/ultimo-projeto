73f7d116bb7f1ed38da531defaa7c209a92b40ef
Thainafadelli <00001096935417sp@al.educacao.sp.gov.br>
2024-11-29 14:48:01 -0300

    Add files via upload

---
+++ respostas.js
@@ -0,0 +1,97 @@
+criaCartao('TEMA', 'Quem é o diretor de Hogwarts durante a maior parte da série?', 'Alvo Dumbledore');
+
+criaCartao('TEMA', 'Qual é o nome completo do Harry?', 'Harry James Potter');
+
+criaCartao('TEMA', 'Qual é a varinha de Harry feita de?', 'Azevinho e pena de fênix');
+
+criaCartao('TEMA', 'Qual é a posição de Harry no time de quadribol?', 'Apanhador');
+
+criaCartao('TEMA', 'Qual é o feitiço usado para desarmar?', 'Expelliarmus');
+
+criaCartao('TEMA', 'Qual é o nome do melhor amigo de Harry?', 'Ronald Weasley');
+
+criaCartao('TEMA', 'Quem é o responsável por cuidar das criaturas mágicas em Hogwarts?', 'Rúbeo Hagrid');
+
+criaCartao('TEMA', 'Qual é a casa de Hermione?', 'Grifinória');
+
+criaCartao('TEMA', 'Qual é a poção usada para mudar de aparência?', 'Poção Polissuco');
+
+criaCartao('TEMA', 'Quem é o herdeiro de Sonserina na Câmara Secreta?', 'Tom Riddle');
+
+criaCartao('TEMA', 'Qual é o patrono de Harry?', 'Um cervo');
+
+criaCartao('TEMA', 'Qual é o animal de estimação de Rony?', 'Um rato chamado Perebas');
+
+criaCartao('TEMA', 'Qual é o nome do mapa que revela todos os segredos de Hogwarts?', 'Mapa do Maroto');
+
+criaCartao('TEMA', 'Quem criou o Mapa do Maroto?', 'Aluado, Rabicho, Almofadinhas e Pontas');
+
+criaCartao('TEMA', 'Qual é o nome da rua onde Harry morava com os Dursley?', 'Rua dos Alfeneiros, número 4');
+
+criaCartao('TEMA', 'Quem é o chefe da casa Sonserina?', 'Severo Snape');
+
+criaCartao('TEMA', 'Qual é o feitiço usado para conjurar luz?', 'Lumos');
+
+criaCartao('TEMA', 'Qual é a plataforma de onde parte o Expresso de Hogwarts?', 'Plataforma 9 ¾');
+
+criaCartao('TEMA', 'Quem é o autor de "Animais Fantásticos e Onde Habitam"?', 'Newt Scamander');
+
+criaCartao('TEMA', 'Quem foi o vencedor do Torneio Tribruxo?', 'Harry Potter');
+
+criaCartao('TEMA', 'Qual é o nome do banco dos bruxos?', 'Gringotes');
+
+criaCartao('TEMA', 'Qual é o nome do elfo doméstico de Harry?', 'Dobby');
+
+criaCartao('TEMA', 'Quem é o primo de Harry?', 'Duda Dursley');
+
+criaCartao('TEMA', 'Quem destrói o diário de Tom Riddle?', 'Harry Potter');
+
+criaCartao('TEMA', 'Qual é o símbolo das Relíquias da Morte?', 'Um triângulo com um círculo dentro e uma linha no meio');
+
+criaCartao('TEMA', 'Quem é o verdadeiro mestre da Varinha das Varinhas?', 'Harry Potter');
+
+criaCartao('TEMA', 'Quem ensina Defesa Contra as Artes das Trevas no primeiro ano de Harry?', 'Professor Quirrell');
+
+criaCartao('TEMA', 'Quem é o guardião das chaves de Hogwarts?', 'Rúbeo Hagrid');
+
+criaCartao('TEMA', 'Quem mata Nagini?', 'Neville Longbottom');
+
+criaCartao('TEMA', 'Qual é o nome do jornal do mundo bruxo?', 'O Profeta Diário');
+
+criaCartao('TEMA', 'Quem é a irmã de Rony?', 'Gina Weasley');
+
+criaCartao('TEMA', 'Qual é a profissão de Arthur Weasley?', 'Trabalha no Ministério da Magia, na Seção de Artefatos de Trouxas');
+
+criaCartao('TEMA', 'Qual é o feitiço usado para repelir dementadores?', 'Expecto Patronum');
+
+criaCartao('TEMA', 'Qual é a primeira Horcrux que Harry destrói?', 'O diário de Tom Riddle');
+
+criaCartao('TEMA', 'Qual é o nome do dragão que Harry enfrenta no Torneio Tribruxo?', 'Rabo-Córneo Húngaro');
+
+criaCartao('TEMA', 'Qual é a planta usada para respirar debaixo d’água?', 'Guelricho');
+
+criaCartao('TEMA', 'Quem é o fantasma da casa da Grifinória?', 'Nick Quase-Sem-Cabeça');
+
+criaCartao('TEMA', 'Quem é o ministro da magia durante a maior parte da série?', 'Cornélio Fudge');
+
+criaCartao('TEMA', 'Qual é o nome completo de Voldemort?', 'Tom Marvolo Riddle');
+
+criaCartao('TEMA', 'Quem dá a Harry a Capa da Invisibilidade?', 'Alvo Dumbledore');
+
+criaCartao('TEMA', 'Qual é o nome da loja de varinhas no Beco Diagonal?', 'Ollivanders');
+
+criaCartao('TEMA', 'Qual é o número do cofre de Harry em Gringotes?', '687');
+
+criaCartao('TEMA', 'Quem é o irmão mais velho de Rony?', 'Gui Weasley');
+
+criaCartao('TEMA', 'Qual é o nome da vassoura que Harry ganha no primeiro ano?', 'Nimbus 2000');
+
+criaCartao('TEMA', 'Quem dirige o Expresso de Hogwarts?', 'O Maquinista de Hogwarts');
+
+criaCartao('TEMA', 'Quem destrói o medalhão de Salazar Sonserina?', 'Rony Weasley');
+
+criaCartao('TEMA', 'Qual é o feitiço para abrir portas trancadas?', 'Alohomora');
+
+criaCartao('TEMA', 'Qual é o animal que representa a casa Lufa-Lufa?', 'Um texugo');
+
+criaCartao('TEMA', 'Quem dá o nome de Edwiges à coruja de Harry?', 'Harry Potter');
